bc57378fb375db51f081b0bbded245f50b799919
Deepanshu <deepanshu127001@gmail.com>
2022-07-19 23:42:44 +0530

    code changed

---
+++ ques1.java
@@ -0,0 +1,17 @@
+/**
+ * ques1
+ * printz
+ */
+// import java.util.*;
+
+public class ques1 {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+    }
+}
