60eef99d53a77e9e348fd1daebb6f2563feb9d12
Deepanshu <deepanshu127001@gmail.com>
2022-07-20 20:17:13 +0530

    ques 7 Digits of number

--- ques7.java
+++ ques7.java
@@ -1,3 +1,4 @@
+
 /*
  * Digits of a number
  * 1. You've to display the digits of a number.
@@ -29,29 +30,30 @@ Sample Output
  */
 import java.util.*;
 import java.lang.Math;
-    
-    public class ques7{
-    public static int getNod(int n){
-      int count=0;
-      while(n!=0){
-        n=n/10;
-        count++;
-      }
-      return count;
+
+public class ques7 {
+    public static int getNod(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
     }
+
     public static void main(String[] args) {
-      int n;
-      Scanner scn=new Scanner(System.in);
-      n=scn.nextInt();
-      int nod=getNod(n);
-      int pow=(int)Math.pow(10,nod-1);
-      // System.out.println(pow);
-      while(pow!=0){
-        int digit=n/pow;
-        System.out.println(digit);
-        n=n%pow;
-        pow/=10;
-      }
-      
-     }
+        int n;
+        Scanner scn = new Scanner(System.in);
+        n = scn.nextInt();
+        int nod = getNod(n);
+        int pow = (int) Math.pow(10, nod - 1);
+        // System.out.println(pow);
+        while (pow != 0) {
+            int digit = n / pow;
+            System.out.println(digit);
+            n = n % pow;
+            pow /= 10;
+        }
+        scn.close();
     }
+}
