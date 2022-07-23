4dbc43baca0b2d8bd6534a1a6b230bda273c3ed7
Deepanshu <deepanshu127001@gmail.com>
2022-07-23 20:59:45 +0530

    pattern 2

--- 02 Patterns/ques2.java
+++ 02 Patterns/ques2.java
@@ -23,11 +23,9 @@ Sample Output
 import java.util.*;
 
 public class ques2{
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n=scn.nextInt();
-
         for(int i=1;i<=n;i++){
             for(int j=n;j>=i;j--){
                 System.out.print("*\t");
