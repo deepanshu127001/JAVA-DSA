f26e7b18b25f15d7e4bfb15bb421c958599efd80
Deepanshu <deepanshu127001@gmail.com>
2022-07-24 23:21:28 +0530

    pattern 6 unsolved

---
+++ 02 Patterns/ques6.java
@@ -0,0 +1,26 @@
+/*
+ * Pattern 6
+Easy
+
+1. You are given a number n.
+2. You've to create a pattern of * and separated by tab as shown in output format.
+
+Constraints
+1 <= n <= 100
+ Also, n is odd.
+  Sample Input
+
+5
+
+Sample Output
+*	*	*	    *	*	*	
+*	*				*	*	
+*						*	
+*	*				*	*	
+*	*	*		*	*	*
+ */
+public class ques6 {
+    public static void main(String[] args) {
+        
+    }
+}
